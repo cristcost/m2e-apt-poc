0321ef41e1dd45e7bd2859649e8b905b3f398858
cristcost <cristiano.costantini@gmail.com>
2015-02-18 16:55:43 +0100

    added a sample use case of class annotation processing that generate a javascript file

--- aptpoc-example/src/main/java/net/cristcost/aptpoc/example/Example.java
+++ aptpoc-example/src/main/java/net/cristcost/aptpoc/example/Example.java
@@ -5,7 +5,21 @@ import net.cristcost.aptpoc.processor.ConvertToJavascript;
 @ConvertToJavascript
 public class Example {
 
-  public int sum(int a, int b) {
+  public static int sum(int a, int b) {
     return a + b;
   }
+
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+
+  // uncomment this method generates a waring on the annotation processor
+  protected static int divide(int a, int b) {
+    return a / b;
+  }
+
+  // uncomment this method to generate an error on the annotation processor
+  // public int substract(int a, int b) {
+  // return a / b;
+  // }
 }
--- aptpoc-processor/src/main/java/net/cristcost/aptpoc/processor/JavaToJsConverterAnnotationProcessor.java
+++ aptpoc-processor/src/main/java/net/cristcost/aptpoc/processor/JavaToJsConverterAnnotationProcessor.java
@@ -1,5 +1,9 @@
 package net.cristcost.aptpoc.processor;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -8,8 +12,13 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.ElementFilter;
+import javax.tools.Diagnostic.Kind;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 
 @SupportedAnnotationTypes({
   "net.cristcost.aptpoc.processor.ConvertToJavascript"
@@ -32,7 +41,62 @@ public class JavaToJsConverterAnnotationProcessor extends AbstractProcessor {
     return true;
   }
 
-  private void processConvertToJavascriptElement(TypeElement convertToJavascriptElement) {
-    // TODO: process the element
+  private List<ExecutableElement> getStaticPublicMethodsOfTypeElement(
+      TypeElement processedTypeElement) {
+
+    final List<ExecutableElement> ret = new ArrayList<ExecutableElement>();
+    List<ExecutableElement> methods =
+        ElementFilter.methodsIn(processedTypeElement.getEnclosedElements());
+
+    for (ExecutableElement method : methods) {
+      if (method.getModifiers().contains(Modifier.STATIC)) {
+        if (method.getModifiers().contains(Modifier.PUBLIC)) {
+          ret.add(method);
+        } else {
+          processingEnv.getMessager().printMessage(
+              Kind.MANDATORY_WARNING,
+              "The processed class includes a method that is not public and will be ignored: "
+                  + method.getSimpleName(), method);
+        }
+      } else {
+        processingEnv.getMessager().printMessage(Kind.ERROR,
+            "The processed class includes a method that is not static: " + method.getSimpleName(),
+            method);
+      }
+    }
+    return ret;
   }
+
+  private void processConvertToJavascriptElement(TypeElement processedTypeElement) {
+    final List<ExecutableElement> methods =
+        getStaticPublicMethodsOfTypeElement(processedTypeElement);
+
+    try {
+
+      String javascriptFileName = processedTypeElement.getSimpleName().toString() + ".js";
+      FileObject javascrptFile =
+          processingEnv.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, "",
+              javascriptFileName);
+
+      Writer writer = javascrptFile.openWriter();
+
+      for (ExecutableElement executableElement : methods) {
+        String functionName = executableElement.getSimpleName().toString();
+        String functionSignature = "/* todo extract the params*/";
+
+        writer.append("var " + functionName + " = function(" + functionSignature + ") {"
+            + System.lineSeparator());
+        writer.append("   // TODO: process and convert to Javascript this function"
+            + System.lineSeparator());
+        writer.append("}" + System.lineSeparator());
+        writer.append(System.lineSeparator());
+
+      }
+      writer.flush();
+      writer.close();
+    } catch (IOException e) {
+      processingEnv.getMessager().printMessage(Kind.ERROR, e.getMessage());
+    }
+  }
+
 }
